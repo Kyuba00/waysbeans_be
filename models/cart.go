3a4a42272fea041bdd69a3cb4da2990949779125
Kyuba00 <akanime1@gmail.com>
2023-03-20 18:06:58 +0700

    deploy

--- models/cart.go
+++ models/cart.go
@@ -1,20 +1,24 @@
 package models
 
 type Cart struct {
-	ID            int             `json:"id" gorm:"primary_key:auto_increment"`
-	ProductID     int             `json:"product_id" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Product       ProductResponse `json:"product"`
-	TransactionID int             `json:"transaction_id" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-	Transaction   Transaction     `json:"-"`
-	Qty           int             `json:"qty"`
-	SubAmount     int             `json:"sub_amount"`
+	ID            int                  `json:"id" gorm:"primary_key:auto_increment"`
+	ProductID     int                  `json:"product_id" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Product       ProductResponse      `json:"product"`
+	TransactionID int                  `json:"transaction_id" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Transaction   Transaction          `json:"-"`
+	Qty           int                  `json:"qty"`
+	SubAmount     int                  `json:"sub_amount"`
+	UserID        int                  `json:"user_id"`
+	User          UsersProfileResponse `json:"user"`
 }
 
 type CartResponse struct {
-	ID        int             `json:"id"`
-	ProductID int             `json:"product_id"`
-	Product   ProductResponse `json:"product"`
-	SubAmount int             `json:"sub_amount"`
+	ID        int                  `json:"id"`
+	ProductID int                  `json:"product_id"`
+	Product   ProductResponse      `json:"product"`
+	SubAmount int                  `json:"sub_amount"`
+	UserID    int                  `json:"user_id"`
+	User      UsersProfileResponse `json:"user"`
 }
 
 func (CartResponse) TableName() string {
