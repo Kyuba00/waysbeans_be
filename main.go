9189bd7bfa428012a7bd8f2a1af0ce83581ff1f9
Kyuba00 <58878873+Kyuba00@users.noreply.github.com>
2023-03-21 16:52:53 +0700

    Update main.go

--- main.go
+++ main.go
@@ -24,7 +24,7 @@ func main() {
 	database.RunMigration()
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
+		AllowOrigins: []string{"https://waysbeans-f0859gxhq-kyuba00.vercel.app"},
 		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
 		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
 	}))
