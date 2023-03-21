081d45fff53b50fed4ab9062affc1274f309f52a
Kyuba00 <akanime1@gmail.com>
2023-03-21 17:51:25 +0700

    changing cors

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	e := echo.New()
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"https://waysbeans-99.vercel.app/", "https://waysbeans-fe-git-main-kyuba00.vercel.app/", "https://waysbeans-f0859gxhq-kyuba00.vercel.app/"},
+		AllowOrigins: []string{"https://waysbeans-99.vercel.app", "https://waysbeans-fe-git-main-kyuba00.vercel.app", "https://waysbeans-f0859gxhq-kyuba00.vercel.app"},
 		AllowMethods: []string{echo.GET, echo.POST, echo.PATCH, echo.DELETE},
 		AllowHeaders: []string{"X-Requested-With", "Content-Type", "Authorization"},
 	}))
